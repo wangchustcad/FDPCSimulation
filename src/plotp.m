eba4f7a96df706c7e682fbd8ad1662e19ec1e458
wangc <wangc@hust.edu.cn>
2021-01-18 08:46:19 +0800

    update script

--- src/plotp.m
+++ src/plotp.m
@@ -1,36 +1,55 @@
 clc
 close all
-clear
+clear all
 
 figure(1)
+i=0;
+j=0;
 
-i=1;
-while(i<=7)
-    clear p
-    index=i-1;
-    filename = sprintf("p%d00.mat",index);
-%     varname = sprintf("p%d",index);
-    load(filename);
+while(j<=1)
+    tail='';
+    if(j==1)
+        tail='k';
+    end
+    while(i<5)
+        clear p
+    
+        subplot(5,2,i*2+j+1);
 
-    subplot(7,1,i);
-    plot(p);
-    title("");
-    xlabel('t');
-    ylabel('');
-    set(gca,'YTick',0:0.5:1);
-    set(gca,'YTickLabel',[0,0.5,1]);
-    set(gca,'YGrid','on');
-%     gca.YAxis.Tick=0:0.5:1;
-%     gca.YAxis.TickLabel=[0,0.5,1];
-%     gca.YAxis.Grid='on';
-    
-    ax=gca;
-    ax.YAxis.MinorTick='on';
-    ax.YAxis.MinorTickValues= 0:0.25:1;
-    ax.YMinorGrid = 'on';
-    ax.MinorGridLineStyle='--';
-    
-    
-    i=i+1;
+        filename = sprintf("p%d00%s.mat",i,tail);
+        load(filename);
+        plot(p);
+        hold on;
+
+        mfilename = sprintf("p%d00m1%s.mat",i,tail);
+        load(mfilename);
+        plot(p);
+
+        title("");
+        xlabel('');
+        ylabel('');
+        xlim([0,25]);
+        ylim([0,1]);
+        if(i~=4)
+            set(gca,'XTickLabel',[]);
+        end
+        
+        set(gca,'YTick',0:0.5:1);
+        set(gca,'YTickLabel',[0,0.5,1]);
+        set(gca,'YGrid','on');
+    %     gca.YAxis.Tick=0:0.5:1;
+    %     gca.YAxis.TickLabel=[0,0.5,1];
+    %     gca.YAxis.Grid='on';
+
+        ax=gca;
+        ax.YAxis.MinorTick='on';
+        ax.YAxis.MinorTickValues= 0:0.25:1;
+        ax.YMinorGrid = 'on';
+        ax.MinorGridLineStyle='--';
+
+        i=i+1;
+    end
+    j=j+1;
+    i=0;
 end
-suptitle('Position')
+% suptitle('Position(m)')
--- src/plotv.m
+++ src/plotv.m
@@ -1,36 +1,65 @@
 clc
 close all
-clear
-
+clear all
+ 
 figure(1)
 
-i=1;
-while(i<=7)
-    clear v
-    index=i-1;
-    filename = sprintf("v%d00.mat",index);
-%     varname = sprintf("p%d",index);
-    load(filename);
+i=0;
+j=0;
+tail='';
+title='Position(m)';
+while(j<=1)
+    if(j==1)
+        tail='k';
+        title='Position()'
+    end
+    while(i<=4)
+        clear v
+        subplot(5,2,i*2+j+1);
 
-    subplot(7,1,i);
-    plot(v);
-    title("");
-    xlabel('t');
-    ylabel('');
-    ylim([-0.6,0.6])
-    set(gca,'YTick',-0.6:0.6:0.6);
-    set(gca,'YTickLabel',[-0.6,0,0.6]);
-    set(gca,'YGrid','on');
-%     gca.YAxis.Tick=0:0.5:1;
-%     gca.YAxis.TickLabel=[0,0.5,1];
-%     gca.YAxis.Grid='on';
-    
-    ax=gca;
-    ax.YAxis.MinorTick='on';
-    ax.YAxis.MinorTickValues= -0.6:0.2:0.6;
-    ax.YMinorGrid = 'on';
-    ax.MinorGridLineStyle='--';
-    
-    i=i+1;
+        filename = sprintf("v%d00%s.mat",i,tail);
+        load(filename);
+        plot(v);
+        hold on;
+        
+        mfilename = sprintf("v%d00m1%s.mat",i,tail);
+        load(mfilename);
+        plot(v);
+        
+        if(i==1)
+            title("");
+        else
+            title("")
+        end
+        
+        xlabel("");
+        
+        ylabel("");
+        xlim([0,25]);
+        ylim([-0.6,0.6]);
+%         if(i==0 & j==0)
+%             legend({'max M','min M'},'Location','northwest','Orientation','horizontal');
+%         end
+        
+        set(gca,'YTick',-0.6:0.6:0.6);
+        set(gca,'YTickLabel',[-0.6,0,0.6]);
+        set(gca,'YGrid','on');
+        if(i~=4)
+            set(gca,'XTickLabel',[]);
+        end
+    %     gca.YAxis.Tick=0:0.5:1;
+    %     gca.YAxis.TickLabel=[0,0.5,1];
+    %     gca.YAxis.Grid='on';
+
+        ax=gca;
+        ax.YAxis.MinorTick='on';
+        ax.YAxis.MinorTickValues= -0.6:0.2:0.6;
+        ax.YMinorGrid = 'on';
+        ax.MinorGridLineStyle='--';
+
+        i=i+1;
+    end
+    i=0;
+    j=j+1;
 end
-suptitle('Velocity')
+% suptitle('Velocity(m/s)')
